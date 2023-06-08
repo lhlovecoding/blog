b7d3766145e62625e13f673bafffb352a46b41c8
李大钊 <459077797@qq.com>
2023-06-08 19:31:52 +0800

    1

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/blog/',
   lang: 'zh-CN',
   title: 'ikun study blog',
   description: 'ikun study blog',
