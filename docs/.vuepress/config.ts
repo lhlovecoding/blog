cc137ee40470fc7b72f66e435417533098df42f1
李大钊 <459077797@qq.com>
2023-06-08 22:31:19 +0800

    1

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,10 +1,19 @@
 import { defineUserConfig } from 'vuepress'
-
+import { defaultTheme } from 'vuepress'
 export default defineUserConfig({
   base: '/blog/',
   lang: 'zh-CN',
   title: 'ikun study blog',
   description: 'ikun study blog',
   head: [['link', { rel: 'icon', type:"x-icon",href: '/images/ikun.ico' }]],
-  pagePatterns:['**/*.md', '!**/README.md', '!.vuepress', '!node_modules']
+  pagePatterns:['**/*.md', '!**/README.md', '!.vuepress', '!node_modules'],
+  theme: defaultTheme({
+    // 默认主题配置
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+    ],
+  }),
 })
