0650f1735241c59d75cfc6dc120de2c7dfbcc554
ikun <459077797@qq.com>
2023-07-13 21:12:21 +0800

    web大纲

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,16 +3,30 @@ import { defaultTheme } from 'vuepress'
 export default defineUserConfig({
   base: '/blog/',
   lang: 'zh-CN',
-  title: 'ikun study blog',
-  description: 'ikun study blog',
-  head: [['link', { rel: 'icon', type:"x-icon",href: '/images/ikun.ico' }]],
-  pagePatterns:['**/*.md', '!**/README.md', '!.vuepress', '!node_modules'],
+  title: 'JavaScript 课程大纲',
+  description: '大纲涵盖二段三段四段60天课程',
+  head: [['link', { rel: 'icon', type: "x-icon", href: '/images/ikun.ico' }]],
+  pagePatterns: ['**/*.md', '!**/README.md', '!.vuepress', '!node_modules'],
   theme: defaultTheme({
     // 默认主题配置
     navbar: [
       {
-        text: '首页',
-        link: '/',
+        text: '二段课程',
+        link: '/guide/二段.md',
+        activeMatch: '/'
+
+      },
+      {
+        text: '三段课程',
+        link: '/guide/三段.md',
+      },
+      {
+        text: '四段课程',
+        link: '/guide/四段.md',
+      },
+      {
+        text: 'Tools',
+        link: '/guide/tools.md',
       },
     ],
   }),
